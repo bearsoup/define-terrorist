a6f9b83c5fea7e5d40979ec7fe7d284373ed9630
Simon Boas <simonboas@gmail.com>
2017-02-02 09:36:00 -0800

    adaptive css

--- sketch.js
+++ sketch.js
@@ -9,14 +9,18 @@ function setup() {
     var grammar = new RiGrammar(json.join('\n'));
     var terrorist = select('#definition');
     var disagree = select('#new-def');
-    disagree.mousePressed(function() {
+    disagree.mouseClicked(function() {
         newdef = grammar.expand();
         terrorist.html(newdef);
+        // prevent default
+        return false;
     });
 
     var tweetdef = select('#tweet-def');
-    tweetdef.mousePressed(function() {
+    tweetdef.mouseClicked(function() {
         var url = "https://twitter.com/intent/tweet?text=" + "A %23terrorist is " + newdef + " www.defineterrorist.us";
         window.open(url);
+        // prevent default
+        return false;
     });
 }
